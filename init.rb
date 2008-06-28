4a28348bc3cd9e3f3172f33dcd973f6cfaee3d88
Nicholas Dainty <nick@npad.co.uk>
2008-06-28 19:43:36 +0100

    never allowing self-closing tags

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 module ::ActionView::Helpers::TagHelper
   alias :xhtml_tag :tag
   
-  def tag(name, options = nil, open = true, escape = true)
-    "<#{name}#{tag_options(options, escape) if options}" + (open ? ">" : " />")
+  def tag(name, options = nil, open = false, escape = true)
+    "<#{name}#{tag_options(options, escape) if options}>"
   end
 end
